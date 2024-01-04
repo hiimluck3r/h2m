7269bd2e188660809bdf20589bf24a6a30bbd11d
hiimluck3r <luck3rinc@yandex.ru>
2024-01-04 17:11:50 +0300

    fix: :poop: tried to fix apt helm source (todo: check for eternal update && upgrade process)

--- setup.sh
+++ setup.sh
@@ -2,11 +2,16 @@
 source /etc/os-release
 echo "Installing dependencies: git, wget, Ansible, Helm, Python, pyyaml"
 
-if [ "$NAME" = "Ubuntu" ] || [ "$NAME" = "Debian" ]; then
-    sudo apt install git wget python ansible helm pip
-    sudo pip install pyyaml
-else
-    sudo pacman -S git wget python ansible helm python-pyyaml
-fi
+curl https://baltocdn.com/helm/signing.asc | gpg --dearmor | sudo tee /usr/share/keyrings/helm.gpg > /dev/null
+apt install apt-transport-https --yes
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/helm.gpg] https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
+apt update
+
+#todo: check for sources (eternal loop on update && upgrade)
+
+apt -y install git wget python3 ansible helm python3-pip && apt -y dist-upgrade
+pip install pyyaml
+
+#sudo pacman -S git wget python ansible helm python-pyyaml
 
 python3 installer.py
