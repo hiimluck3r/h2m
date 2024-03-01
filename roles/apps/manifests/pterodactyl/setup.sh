7af27cf54789c9e3df4b078633c08e25dd23aab6
hiimluck3r <luck3rinc@yandex.ru>
2024-03-01 13:01:13 +0300

    fix: :ambulance: hotfix #3

---
+++ roles/apps/manifests/pterodactyl/setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+#./setup.sh firstname lastname email password username
+kubectl cp panel.conf $(kubectl get pods | grep kubectyl-panel | awk '{print $1}'):/etc/nginx/http.d/ --;
+kubectl exec $(kubectl get pods | grep kubectyl-panel | awk '{print $1}') -- php artisan p:user:make --admin=1 --name-first="$1" --name-last="$2" --email="$3" --password="$4" --username="$5" && reboot;
