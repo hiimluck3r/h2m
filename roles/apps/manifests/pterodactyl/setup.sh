171a8efad7386ab241e9e814b0ed911be55da6c3
hiimluck3r <luck3rinc@yandex.ru>
2024-03-01 17:38:40 +0300

    revert: :rewind: Revert pterodactyl deployment

--- roles/apps/manifests/pterodactyl/setup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-#./setup.sh firstname lastname email password username
-kubectl cp panel.conf $(kubectl get pods | grep kubectyl-panel | awk '{print $1}'):/etc/nginx/http.d/ --;
-kubectl exec $(kubectl get pods | grep kubectyl-panel | awk '{print $1}') -- php artisan p:user:make --admin=1 --name-first="$1" --name-last="$2" --email="$3" --password="$4" --username="$5";
-kubectl delete pod $(kubectl get pods | grep kubectyl-panel | awk '{print $1}');
