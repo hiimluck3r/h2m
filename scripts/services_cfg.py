6bc08617fbeabefede25f2966e8b80e9be2d394f
hiimluck3r <luck3rinc@yandex.ru>
2024-03-18 17:00:18 +0300

    🐞 fix: fix listeners naming and tls secret names, use item instead of invalid domain variable

--- scripts/services_cfg.py
+++ scripts/services_cfg.py
@@ -56,7 +56,7 @@ for each in services:
             task_data = task_file.read()
         
         current_domain = re.findall(r'domain: .*', task_data)[0]
-        print(f'Current domain for {each["name"]} service: {current_domain}')
+        print(f'Current {each["name"]} service {current_domain}')
         print(f'Configure domain for {each["name"]} service: ')
         domain = select_multiple(config_info['domains'], tick_character='x', maximal_count=1, ticked_indices=[0])
         domain = domain[0]
